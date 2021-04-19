17af0262ec3fb1fa6513ddabd048f565081a8abd
dhia-gharsallaoui <74959435+dhia-gharsallaoui@users.noreply.github.com>
2021-04-19 16:07:44 +0200

    Update parameters.py

--- Vgg19/parameters.py
+++ Vgg19/parameters.py
@@ -2,15 +2,16 @@ import torch.nn as nn
 
 data_path='/home/dhia/data/train'
 test_path='/home/dhia/data/test'
-IMG_SIZE = 224
-batch_size = 32
+IMG_SIZE = 224    # Data transform size to (IMG_SIZE,IMG_SIZE)
+batch_size = 10   # Loader Batch size
 
 #torch parameters
 
 #i add this to be able to use cuda when u have it
 ## run the following to know if u can use cuda
 ### dev = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-epochs = 10
 dev='cpu'
+#learning parameters
+epochs = 10
 criteria= nn.CrossEntropyLoss()
 learning_rate=0.001
